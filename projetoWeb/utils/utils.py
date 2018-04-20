e97fc122cdac5c172d16e794d3c977f50aaa54e9
aluno <Aluno@salas.aulas>
2018-04-19 22:05:58 -0300

    ac LP2

--- projetoWeb/utils/utils.py
+++ projetoWeb/utils/utils.py
@@ -1,3 +1,22 @@
+from difflib import SequenceMatcher
+
+
 def calculaMediaFinal(ac, Prova):
     media = 0.6 * ac + 0.4 * Prova
     return media
+
+
+
+def verificaCopia(texto1, texto2):
+    var = int(SequenceMatcher(None, texto1, texto2).ratio())
+
+    if(var >= 0.8):
+       return True
+    else:
+       return False
+        
+
+
+
+
+print(verificaCopia('Tassio','Tassio'))
